f5229b400e1c9c64d8866fa96f1fad46ad94cdcf
wissemslim <wissemslim.w@gmail.com>
2023-11-04 22:06:19 +0100

    fix

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
             // Allow for specific domains to have access or * for all
             {
               key: "Access-Control-Allow-Origin",
-              value: "*",
+              value: process.env.NEXT_PUBLIC_BASE_URL,
               // DOES NOT WORK
               // value: process.env.ALLOWED_ORIGIN,
             },
