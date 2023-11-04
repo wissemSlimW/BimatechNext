4694cc79caebf1cdf7cc6cec4e0c99ba520bf2ad
wissemslim <wissemslim.w@gmail.com>
2023-11-04 23:05:38 +0100

    next config

--- next.config.js
+++ next.config.js
@@ -1,33 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async headers() {
-      return [
-        {
-          // Routes this applies to
-          source: "/(.*)",
-          // Headers
-          headers: [
-            // Allow for specific domains to have access or * for all
-            {
-              key: "Access-Control-Allow-Origin",
-              value: "*",
-              // DOES NOT WORK
-              // value: process.env.ALLOWED_ORIGIN,
-            },
-            // Allows for specific methods accepted
-            {
-              key: "Access-Control-Allow-Methods",
-              value: "GET, POST, PUT, DELETE, OPTIONS",
-            },
-           
-             {
-              key: "Cache-Control",
-              value: "private, no-cache, no-store, max-age=0, must-revalidate",
-            },
-          ],
-        },
-      ];
-    },
-  };
-  
-  module.exports = nextConfig;
+  async headers() {
+    return [
+      {
+        // Routes this applies to
+        source: "/api/(.*)",
+        // Headers
+        headers: [
+          // Allow for specific domains to have access or * for all
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+            // DOES NOT WORK
+            // value: process.env.ALLOWED_ORIGIN,
+          },
+          // Allows for specific methods accepted
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          // Allows for specific headers accepted (These are a few standard ones)
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
